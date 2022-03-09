ec8a363b47370d7d79fa2520e1ac542ced613d2e
Matthias Simons <62206220+MatthiasSimons@users.noreply.github.com>
2022-03-09 21:19:37 +0000

    updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='imu_iot',
+    version='',
+    packages=['src', 'src.userrelated', 'src.processrelated'],
+    url='',
+    license='',
+    author='matthiassimons',
+    author_email='',
+    description=''
+)
